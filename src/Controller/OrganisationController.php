dc4037c07a689fcd74e44380309319f5aa741fb8
Adrien Morin <morin.adrien@gmail.com>
2023-09-28 12:56:53 +0200

    MakeCRUD

--- src/Controller/OrganisationController.php
+++ src/Controller/OrganisationController.php
@@ -13,7 +13,7 @@ use App\Repository\OrganisationRepository;
 
 class OrganisationController extends AbstractController
 {
-    #[Route('/organisation', name: 'organisation_index')]
+    #[Route('/organisation', name: 'organisation_index', methods: 'GET')]
     public function index(OrganisationRepository $organisationRepository): Response
     {
         $organisations = $organisationRepository->findAll();
@@ -23,15 +23,16 @@ class OrganisationController extends AbstractController
         ]);
     }
 
-    /*
-        use App\Entity\Organisation;
-        use App\Form\OrganisationType;
-        use Doctrine\ORM\EntityManagerInterface;
-        use Symfony\Component\HttpFoundation\Request;
-     * */
+    #[Route('/organisation/{id}', name: 'organisation_show', requirements: ['id' => '\d{1,3}'], methods: 'GET')]
+    public function show(Organisation $organisation): Response
+    {
+        return $this->render('organisation/show.html.twig', [
+            'organisation' => $organisation
+        ]);
+    }
 
-    #[Route('/organisation/new', name: 'organisation_new')]
-    public function new(Request $request, EntityManagerInterface $manager)
+    #[Route('/organisation/new', name: 'organisation_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
     {
         $organisation = new Organisation();
         $form = $this->createForm(OrganisationType::class, $organisation);
@@ -41,12 +42,49 @@ class OrganisationController extends AbstractController
             $manager->persist($organisation);
             $manager->flush();
 
+            $this->addFlash('success', 'Création de votre association réalisée!');
+
             return $this->redirectToRoute('organisation_index');
         }
 
         return $this->render('organisation/new.html.twig', [
             'form' => $form
         ]);
+    }
+
+    #[Route('/organisation/update/{id}', name: 'organisation_update', requirements: ['id' => '\d{1,3}'], methods: ['GET', 'POST'])]
+    public function update(Organisation $organisation, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(OrganisationType::class, $organisation);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+
+            $this->addFlash('success', "Modification de l'association {$organisation->getName()} réalisée!");
+
+            return $this->redirectToRoute('organisation_update', [
+                'id' => $organisation->getId()
+            ]);
+        }
+
+        return $this->render('organisation/update.html.twig', [
+            'form' => $form,
+            'organisation' => $organisation
+        ]);
+    }
+
+    #[Route('/organisation/delete/{id}/{token}', name: 'organisation_delete', requirements: ['id' => '\d{1,3}'], methods: 'GET')]
+    public function delete(Organisation $organisation, string $token, EntityManagerInterface $manager)
+    {
+        if ($this->isCsrfTokenValid('delete' . $organisation->getId(), $token)) {
+            $manager->remove($organisation);
+            $manager->flush();
+
+            $this->addFlash('success', "Suppression de l'association {$organisation->getName()} réalisée!");
+        }
+
+        return $this->redirectToRoute('organisation_index');
     }
 }
 
